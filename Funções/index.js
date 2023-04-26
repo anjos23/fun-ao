edf3f8c77cbaca326c27bb44b86e49225554b44e
Thales.Anjos <129232556+anjos23@users.noreply.github.com>
2023-04-26 19:40:03 -0300

    Add files via upload

--- Funções/index.js
+++ Funções/index.js
@@ -1,5 +1,5 @@
 //d) Faça uma função que recebe uma mensagem (string) como parâmetro e imprima o tamanho dessa mensagem, juntamente com uma versão dela em letras maiúsculas.
-function exibirMensagem(texto){
+ /* function exibirMensagem(texto){
     return texto.toUpperCase() +"-" + texto.length;    
 
 }
@@ -14,22 +14,22 @@ console.log(exibirMensagem('vamos gremio'))
    }
 
 
-   function subtração(){
+   function subtração(n1, n2){
     return n1 - n2;
    }
 
-   function divisão (){
+   function divisão (n1, n2){
     return n1 / n2 ;
    }
 
-   function multiplicação(){
+   function multiplicação(n1, n2){
    return  n1 * n2 ;
    }
    let somaDois = soma(2, 3)
    let subDois = subtração(2, 3)
    let divDois = divisão(2, 3)
    let multDois = multiplicação(2, 3)
-   console,log('Numeros inseridos: ' + 2 + ', ' + 3)
+   console.log('Numeros inseridos: ' + 2 + ', ' + 3)
    console.log( 'soma: '+ somaDois)
    console.log('subtração: '+ subDois)
    console.log('divisão: '+ divDois)
@@ -41,4 +41,11 @@ console.log(exibirMensagem('vamos gremio'))
    function hipo(cat1, cat2){
     return Math.sqrt(Math.pow(cat1, 2)+ Math.pow(cat2, 2))
 
-   }console.log(hipo(3,4).toFixed(2));
+   }console.log(hipo(3,4).toFixed(2));  */
+let variavel = 0
+
+   function executa(variavel){
+    return variavel = 356;
+   }
+   alert(variavel)
+   executa(5)
