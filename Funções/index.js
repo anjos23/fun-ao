ae7b3aa38f5faca9765f91e0597768bf507f2bbf
Thales.Anjos <129232556+anjos23@users.noreply.github.com>
2023-04-26 21:23:32 -0300

    Add files via upload
    corrigindo exercicios

--- Funções/index.js
+++ Funções/index.js
@@ -42,10 +42,57 @@ console.log(exibirMensagem('vamos gremio'))
     return Math.sqrt(Math.pow(cat1, 2)+ Math.pow(cat2, 2))
 
    }console.log(hipo(3,4).toFixed(2));  */
-let variavel = 0
+/*
+   let variavel = 0
 
    function executa(variavel){
     return variavel = 356;
    }
    alert(variavel)
-   executa(5)
+   executa(5) */
+   //Escreva as funções explicadas abaixo:
+   //a) A função não deve receber nenhum parâmetro e deve imprimir uma mensagem falando algumas informações sobre você, como: 
+  
+   function imprimir(){
+      return "Eu sou Thales, tenho 33 anos, moro em São Leopoldo e sou estudante."
+
+   }
+   console.log(imprimir("Eu sou Thales, tenho 33 anos, moro em São Leopoldo e sou estudante."))
+
+
+function informacoesPessoa(nome, idade, cidade, profissao){
+   return `ola ${nome}, tenho ${idade}, moro em ${cidade} e sou ${profissao}`
+}
+
+let nomePessoa = prompt('nome:')
+let idadePessoa = Number(prompt('idade: '))
+alert (informacoesPessoa(nomePessoa, idadePessoa,'saoleopoldo',"estudante" ))
+
+
+function retornaMaior (num1, num2){
+return num1 >= num2
+
+}
+alert(retornaMaior(Number(1), Number(3)))
+
+function ePar(num){
+   return num % 2 === 0;
+
+}
+alert (ePar(250))
+
+
+
+let desafioA= (meuTime) => {//MODELO ARROW FUNCTION
+   return meuTime;
+
+
+}
+alert (desafioA('nacional'))
+
+let desafioB =() => {
+
+
+   
+}
+
